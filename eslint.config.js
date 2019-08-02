163ec9eba758c6ecbfb9dd31585bcdf1200157c7
Ben Edwards <hello@benedfit.com>
2019-08-02 14:47:26 +0100

    Adds REACT plugin recommended

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,7 @@ module.exports = {
     },
     extends: [
         'eslint:recommended',
+        'plugin:react/recommended',
         'standard',
         'standard-react',
         'plugin:prettier/recommended',
